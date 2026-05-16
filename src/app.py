14e1fb700ccc86f4b9b233d2864d2090de23eb33
AbdurrhmanSabry <abdurrhmansabry@gmail.com>
2026-05-16 15:00:07 +0300

    update kaniko docker config

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': f'You are doing great, little human! <3 From the Python app running on Kubernetes. built from Github Actions CI/CD pipeline. Keep up the good work! Commit SHA !2 from self hosted runners try 1',
+        'message': f'You are doing great, little human! <3 From the Python app running on Kubernetes. built from Github Actions CI/CD pipeline. Keep up the good work! Commit SHA !2 from self hosted runners try 2',
         'deployed_on': 'kubernetes'
     })
 
